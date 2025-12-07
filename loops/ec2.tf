e7f322b5e2b64ef05edf0a269847be39966ece5e
vijaykrishna.b900@gmail.com <vijaykrishna.b900@gmail.com>
2025-12-07 17:42:46 +0530

    creating instances using count for multiple instances using count

---
+++ loops/ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "vm" {
+  ami = var.ami_id
+  count = 3
+  instance_type = var.environment == "dev"?"t3.micro":"t3.small"
+
+  tags = merge(var.common_tags,
+    {
+    Name = var.tags
+  }
+)
+}
+resource "aws_security_group" "Creating_allow_all_tls_security_group" {
+  name = "Creating_allow_all_tls_security_group"
+  description = "Creating_allow_all_tls_security_group"
+
+  ingress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = var.protocol
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = var.ipv6_cidr_blocks
+  }
+  egress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = var.protocol
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = var.ipv6_cidr_blocks
+  }
+   tags = {
+        name = "Creating_allow_all_tls_security_group"
+   } 
+}
---
+++ loops/variable.tf
@@ -0,0 +1,34 @@
+variable "ami_id" {
+  type = string
+  default = "ami-09c813fb71547fc4f"
+  description = "AMIAMI ID of join devops RHEL9_for Terraform practice"
+}
+variable "environment" {
+  default = "dev"
+}
+#variable "instance_type" {
+#  default = var.environment == "dev"?"t3.micro":"t3.small"
+#}
+variable "cidr_blocks" {
+  default =  ["0.0.0.0/0"]
+}
+variable "ipv6_cidr_blocks" {
+  default = ["::/0"]
+}
+variable "from_port" {
+  default = 0
+}
+variable "to_port" {
+  default = 0
+}
+variable "protocol" {
+  default = "-1"
+}
+variable "common_tags" {
+  default = {
+    environment = "dev"
+  }
+}
+variable "tags" {
+  default = "sreejahost"
+}
