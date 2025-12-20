b9570b4ff21ccef95b6f0fd76ab1da86e7f45401
vijaykrishna.b900@gmail.com <vijaykrishna.b900@gmail.com>
2025-12-20 17:44:45 +0530

    created VM using for lenght

---
+++ functions/ec2.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "vm" {
+  #for_each = var.instance_name
+  ami = var.ami_id
+  #for_each = toset(var.instance_name)
+  count = length(var.instance_name)
+  #instance_type = each.value
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ aws_security_group.Creating_allow_all_tls_security_group.id ]
+  #instance_type = var.environment == "dev"?"t3.micro":"t3.small"
+
+  tags = {
+    Name = "${var.instance_name[count.index]}.${var.domain_name}" # added interpolation here
+    #Name = each.key
+  }
+}
+
+resource "aws_security_group" "Creating_allow_all_tls_security_group" {
+  name = "Creating_allow_all_tls_security_group"
+  description = "Creating_allow_all_tls_security_group"
+
+  ingress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = var.protocol
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = var.ipv6_cidr_blocks
+  }
+  egress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = var.protocol
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = var.ipv6_cidr_blocks
+  }
+   tags = {
+        name = "Creating_allow_all_tls_security_group"
+   } 
+}
---
+++ functions/route53.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_record" "https" {
+  #for_each = aws_instance.vm
+  count = length(var.instance_name)
+  zone_id = var.zone_id
+  #name = "${each.key}.${var.domain_name}" # adding DNS and Public IP mysql.sreeja.site using for loop it uses key and value pair
+  name    = "${var.instance_name[count.index]}.${var.domain_name}" # adding DNS and Public IP mysql.sreeja.site
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.vm[count.index].private_ip]
+  #records = [ each.value.private_ip ]
+}
